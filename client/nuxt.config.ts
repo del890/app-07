cd2745e065358c9b61b20d06a2f778a949e2f9f3
Douglas <douglas@ccmdesign.ca>
2026-04-29 19:43:57 -0700

    fix: update nitro configuration and correct publish path in netlify.toml

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  nitro: {
+    // ensure publicDir is not changed away from '.output/public'
+    publicDir: '.output/public'
+  },
 
   ssr: false,
 
