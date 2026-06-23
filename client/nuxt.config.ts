a842db45707636826ae99e286c6fae6b0ed25e7b
Lívia Simões <livias.simoes@yahoo.com.br>
2026-06-22 21:17:34 -0700

    Add log for nuxt config test

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+console.log("DENTRO DO DOCKER - API_BASE:", process.env.NUXT_PUBLIC_API_BASE)
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
